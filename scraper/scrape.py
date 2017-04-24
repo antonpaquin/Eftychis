cb0c93d9f25b69c4834ccf4b548180f88617b01b
Anton <antonpaquin@gmail.com>
2017-04-24 22:09:42 +0000

    Use scrape.py + crontab, hopefully this is stable and wont stop scraping every few hours

---
+++ scraper/scrape.py
@@ -0,0 +1,29 @@
+import collector
+import store
+
+from geocodes import geocodes
+
+def fullcodes():
+    global numScraped
+
+    for geocode in geocodes:
+        # This starts us out at whenever we started the scraper
+        li = collector.getTweets(geocode, None)
+        # Stash them in ES
+        store.putMany(li, geocode)
+        # Track number we got
+        numScraped += len(li)
+
+# track how many tweets we've scraped
+numScraped = 0
+scrapeLimit = 2000000
+
+# use some file IO to keep track of things in progress
+def doLogging():
+    with open('log.txt', 'a') as log_f:
+        log_f.write('Scraped: {} \n'.format(str(numScraped)))
+        log_f.write('Err: {} \n'.format(str(store.exceptionCount)))
+        log_f.write('\n')
+
+fullcodes()
+doLogging()
