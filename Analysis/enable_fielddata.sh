060b5b0b3f91ffe7ff1a3ad80d0a98ffaf7e0fa6
Anton <antonpaquin@gmail.com>
2017-05-04 19:54:07 +0000

    updated mapping to accept searches on field data

--- Analysis/enable_fielddata.sh
+++ Analysis/enable_fielddata.sh
@@ -1,2 +1,2 @@
-curl -XPUT 'http://127.0.0.1:9200/twdata/_mapping/content?pretty' -d @json/enable_fielddata.json > results/enable_fielddata.json &&
+curl -XPUT 'http://127.0.0.1:9200/twdata/_mappings/tweet?pretty' -d @json/enable_fielddata.json > results/enable_fielddata.json &&
 cat results/enable_fielddata.json
