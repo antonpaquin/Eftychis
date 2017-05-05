e81780270ee260a7ed953fb80faf35cdcf7d89c1
Anton <antonpaquin@gmail.com>
2017-05-05 05:49:17 +0000

    gotta go fast

--- Analysis/runTweetScores.py
+++ Analysis/runTweetScores.py
@@ -5,36 +5,97 @@ import code
 
 es = Elasticsearch()
 
+batchSize = 100
+scrollContext = '10m'
+
 last_scroll_id = ''
 
 def getIdBatch():
-    j = search(
+    global last_scroll_id
+    
+    j = es.scroll(
+        scroll=scrollContext,
+        scroll_id=last_scroll_id
+    )
+    last_scroll_id = j['_scroll_id']
+    ids = [a['_id'] for a in j['hits']['hits']]
+    return ids
+
+def getFirstIdBatch():
+    global last_scroll_id
+    
+    j = es.search(
         index='twdata',
         doc_type='tweet',
-        body=getNextScroll(),
-        scroll="1m",
-        size=10
+        body={
+            'query': {
+                "bool": {
+                    "must_not": {
+                        "exists" : { "field" : "happyScore" }
+                        }
+                    },
+                "range": {
+                    "time": {
+                        "gte": 1488344400000,
+                        "lte": 1493697600000,
+                        "format": "epoch_millis"
+                        }
+                    }
+                }
+            },
+        scroll=scrollContext,
+        size=batchSize
     )
-    return j
+    last_scroll_id = j['_scroll_id']
+    ids = [a['_id'] for a in j['hits']['hits']]
+    return ids
 
-def getNextScroll():
-    return {
-        "size": 100,
-        "query": {
-            "match_all" : {}
+def getTermedTweet(ids):
+    j = es.mtermvectors(
+        index='twdata',
+        doc_type='tweet',
+        ids=','.join(ids),
+        fields='content',
+        offsets=False,
+        field_statistics=False,
+        payloads=False,
+        positions=False,
+        realtime=False
+    )
+    return [(a['_id'], list(a['term_vectors']['content']['terms'].keys())) for a in j['docs'] if a['term_vectors'] != {}]
+
+def scoreAndUpdateTweet(tweet):
+    tid = tweet[0]
+    score = tweetScore(tweet[1])
+    es.update(
+        index='twdata',
+        doc_type='tweet',
+        id=tid,
+        body={
+            'doc': {
+                'happyScore': score
+            }
         },
-        "scroll_id": last_scroll_id
-    }
+        retry_on_conflict=10
+    )
 
-'''
-POST /twdata/tweet/_mtermvectors
-{
-    "ids" : ["1", "2"],
-    "parameters": {
-        "fields": [
-                "content"
-        ]
-    }
-}
-'''
-code.interact(local=locals())
+def step():
+    if last_scroll_id == '':
+        ids = getFirstIdBatch()
+    else:
+        ids = getIdBatch()
+
+    tweets = getTermedTweet(ids)
+
+    for tweet in tweets:
+        scoreAndUpdateTweet(tweet)
+
+
+def run():
+    i = 0
+    while i < 24000000:
+        step()
+        i += batchSize
+        print(i)
+
+run()
